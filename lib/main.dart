7558bf232f5c6b3d2284e09411af82b09408e09a
Eric Lekwa <lekwaeric@yahoo.com>
2022-05-18 07:46:10 +0100

    Made Amin changes

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class IndexState extends State<Index> {
               javascriptMode: JavascriptMode.unrestricted,
               onWebViewCreated: (WebViewController webViewController) async {
                 webViewController = webViewController;
-                await webViewController.loadUrl('https://vistacks.com/app');
+                await webViewController.loadUrl('https://vistacks.com/app/admin');
               },
               onPageStarted: (String url) {
                 setState(() {
